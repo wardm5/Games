09f5dd3f99730718dfde876db8a072a8a0ad6fbc
Misha Ward <wardm5@uw.edu>
2019-07-16 20:16:51 -0700

    updated game logic, included end functionality

--- Chutes_and_Ladders/main.java
+++ Chutes_and_Ladders/main.java
@@ -5,13 +5,13 @@ public class main {
     private static int[] board;
     private static ArrayList<Person> players;
     public static void main(String[] args) {
+        intro();
         startGame();
     }
     public static void startGame() {
-        intro();
         setupGame();
-        playGame();
-        // End();
+        int winner = playGame();
+        end(winner);
     }
     public static void intro() {
         System.out.println("Welcome to Chutes and Ladders! ");
@@ -38,10 +38,7 @@ public class main {
     public static void setupBoard() {
         board = new int[101];
         try {
-            // URL url = getClass().getResource("chutes_ladders.txt");
-            // File file = new File(url.getPath());
             File file = new File("chutes_ladders.txt");
-            // File file = new File(".//Games//Chutes_and_Ladders//chutes_ladders.txt");
             Scanner fileScan = new Scanner(file);
             while(fileScan.hasNextLine()) {
                 int index = fileScan.nextInt();
@@ -57,29 +54,26 @@ public class main {
     public static void setupPlayers() {
         players = new ArrayList<>();
         for (int i = 0; i < numOfPlayers; i++) {
-            setupPlayersHelper(i);
+            System.out.print("Please enter player " + (i+1) + "'s name:    ");
+            Scanner strScan = new Scanner(System.in);
+            String name = strScan.next();
+            Person temp = new Person(name);
+            players.add(temp);
         }
-    }
-    public static  void setupPlayersHelper(int i) {
-        System.out.print("Please enter player " + (i+1) + "'s name:    ");
-        Scanner strScan = new Scanner(System.in);
-        String name = strScan.next();
-        Person temp = new Person(name);
-        players.add(temp);
     }
     public static int playGame() {
         int maxPosition = 0;
-        while (maxPosition < 100) {
+        while (maxPosition <= 100) {
             for (int i = 0; i < numOfPlayers; i++) {
                 Person temp = players.get(i);
                 String name = temp.getName();
                 int roll = diceRoll(name);
-                int position = temp.getPosition() + roll;
-                position = checkPosition(position, name) + position;
+                int position = temp.getPosition();
+                position = checkPosition(position, roll, name);
                 temp.setPosition(position);
                 if (maxPosition < position)
                     maxPosition = position;
-                else if (position == 100)
+                if (position == 100)
                     return i;  // return winner index
             }
         }
@@ -90,20 +84,45 @@ public class main {
         System.out.println(name + " rolled a " + roll + "!");
         return roll;
     }
-    public static int checkPosition(int position, String name) {
+    public static int checkPosition(int position, int roll, String name) {
+        if (roll + position > 100) {
+            System.out.println(name + ", you did not get to 100 this turn...");
+            System.out.println("Your current position is:    " + position);
+            newLine();
+            return position;
+        }
+        position = roll + position;
+        int newPosition = board[position] + position;
         if (board[position] > 0) {
-            System.out.println("Lucky you " + name + ", you landed on a slide, you will advance " + board[position] + " spaces!");
-            System.out.println("Your new position is:    " + (board[position] + position));
+            System.out.println("Lucky you " + name + ", you landed on a ladder, you will advance " + board[position] + " spaces!");
+            System.out.println("Your new position is:    " + newPosition);
         } else if (board[position] < 0) {
             System.out.println("Oh nooo " + name + "! You landed on a slide, you go backwards " + (-board[position]) + " spaces!");
-            System.out.println("Your new position is:    " + (board[position] + position));
+            System.out.println("Your new position is:    " + newPosition);
         } else {
             System.out.println(name + ", you did not land on a shoot or ladder this turn...");
-            System.out.println("Your new position is:    " + (board[position] + position));
+            System.out.println("Your new position is:    " + newPosition);
         }
         newLine();
-        if (position + board[position] > 100)
-            return position;
-        return board[position];
+        return newPosition;
     }
+    public static void end(int i) {
+        System.out.println("Congratulations " + players.get(i).getName() + ", you reached 100 and won the game!");
+        newLine();
+        System.out.println("Do you want to play again? Enter 'yes' or 'no'.");
+        Scanner scan = new Scanner(System.in);
+        String response = scan.next();
+        while (!response.toLowerCase().equals("yes") && !response.toLowerCase().equals("no")) {
+            System.out.println("Please enter a valid response of either 'yes' or 'no'.");
+            response = scan.next();
+            System.out.println(response.toLowerCase().equals("yes"));
+        }
+        if (response.toLowerCase().equals("yes")) {
+            startGame();
+        } else {
+            System.out.println("Goodbye! Hope you had fun! ");
+            return;
+        }
+    }
+
 }
