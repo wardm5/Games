e74acfefac6abd3da476e19982c77481e94c029b
Misha Ward <wardm5@uw.edu>
2019-09-27 14:04:07 -0500

    added new game, Fingers

---
+++ FingerGame/Player.java
@@ -0,0 +1,63 @@
+public class Player {
+    String name;
+    int fingerCountLeft;
+    int fingerCountRight;
+    public Player() {
+        this.name = "";
+        this.fingerCountLeft = 1;
+        this.fingerCountRight = 1;
+    }
+    public Player(String name) {
+        this.name = name;
+        this.fingerCountLeft = 1;
+        this.fingerCountRight = 1;
+    }
+    public void showFingerCounts() {
+        System.out.println("RIGHT hand has " + fingerCountRight + " fingers up. ");
+        System.out.println("LEFT hand has " + fingerCountLeft + " fingers up. ");
+        // System.out.print("LEFT HAND:   ");
+        // for (int i = 0; i < fingerCountLeft; i++) {
+        //     System.out.print("|");
+        // }
+        // System.out.print("     RIGHT HAND:   ");
+        // for (int i = 0; i < fingerCountRight; i++) {
+        //     System.out.print("|");
+        // }
+        System.out.println();
+        System.out.println();
+    }
+    public void updatePlayerName(String name) {
+        this.name = name;
+    }
+    public void updateFingers(int n, boolean leftOrRight) {  // left = false, right = true
+        if (leftOrRight) {
+            fingerCountRight += n;
+            if (fingerCountRight >= 5)
+                fingerCountRight = 0;
+        } else {
+            fingerCountLeft += n;
+            if (fingerCountLeft >= 5)
+                fingerCountLeft = 0;
+        }
+    }
+    public int attack(boolean leftOrRight) {
+        if (leftOrRight && fingerCountRight > 0)
+            return fingerCountRight;
+        else if (!leftOrRight && fingerCountLeft > 0)
+            return fingerCountLeft;
+        else return -1;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public boolean hasLost() {
+        return (this.fingerCountLeft == 0 && this.fingerCountRight == 0);
+    }
+    public int getLeftFingerCount() {
+        return fingerCountLeft;
+    }
+    public int getRightFingerCount() {
+        return fingerCountRight;
+    }
+
+}
---
+++ FingerGame/main.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class main {
+    private static Player[] players = new Player[2];
+    public static void main(String[] args) {
+
+        System.out.println("hello world! Testing!");
+        start();
+    }
+    public static void start() {
+        System.out.println("Welcome to the finger game! ");
+        System.out.print("How many players do you want to name? (min of 0, max of 2) ");
+        Scanner scan = new Scanner(System.in);
+        int numOfPlayers = scan.nextInt();
+        while (numOfPlayers > 2 || numOfPlayers < 0) {
+            System.out.print("How many players do you want to name? (min of 0, max of 2) ");
+            numOfPlayers = scan.nextInt();
+        }
+        for (int i = 0; i < 2; i++) {
+            if (numOfPlayers > 0) {
+                System.out.print("What would you like to name Player " + (i+1) + "? ");
+                Scanner test = new Scanner(System.in);
+                String tempName = test.nextLine();
+                Player player1 = new Player(tempName);
+
+                numOfPlayers--;
+                players[i] = player1;
+
+            } else {
+                Player player1 = new Player("Player " + (i+1));
+                players[i] = player1;
+            }
+        }
+        int turn = 0;
+        while (!players[0].hasLost() && !players[1].hasLost()) {
+            if (turn % 2 == 0) {
+                turn(0, 1);
+            } else {
+                turn(1, 0);
+            }
+            turn++;
+        }
+        if (players[0].hasLost())
+            System.out.println("Player 2 has won!! ");
+        else
+            System.out.println("Player 1 has won!!");
+        // end game announcement
+    }
+    public static void turn(int p1, int p2) {
+        System.out.println(players[p1].getName() + ", it is your turn!!");
+        System.out.println("Your finger counts are below: ");
+        players[p1].showFingerCounts();
+        System.out.println("Your opponent's finger count is: ");
+        players[p2].showFingerCounts();
+
+        if (   players[p1].getLeftFingerCount() > 0 && players[p1].getLeftFingerCount() % 2 == 0
+            && players[p1].getRightFingerCount() > 0 && players[p1].getRightFingerCount() % 2 == 0) {
+            System.out.println("You have the option to split! Do you want to do this? (enter 'yes' or 'no')");
+            //user input
+            Scanner temp = new Scanner(System.in); // need to finish...
+
+        }
+
+        System.out.print("Which hand do you choose to attack with? (enter 'right' or 'left')  ");
+        Scanner scan = new Scanner(System.in);
+        String temp = scan.nextLine();
+        int attack = 0;
+        while (!temp.toLowerCase().equals("left") && !temp.toLowerCase().equals("right")) {
+            System.out.print("Which hand do you choose to attack with? (enter 'right' or 'left')  ");
+            temp = scan.nextLine();
+        }
+
+        if (temp.toLowerCase().equals("left"))
+            attack = players[p1].attack(false);
+        else
+            attack = players[p1].attack(true);
+        // System.out.print("Which of your opponent's hands do you want to attack?");
+
+
+        if (temp.toLowerCase().equals("left"))
+            players[p2].updateFingers(attack, false);
+        else
+            players[p2].updateFingers(attack, true);
+    }
+}
